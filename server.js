54fd3d934b9ec4d62cbaea8bb53154e910ecece2
TraBee <tra8brown@gmail.com>
2021-07-25 22:40:41 -0600

    sql database connection

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 //express middleware
 app.use(express.urlencoded({ extrended: false }));
 app.use(express.json());
 
-//Default response for any other request (Not Found)
+//connect to database
+const db = mysql.createConnection({
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: 'Biihbitoodni2017!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+//Default response for any other request (Not Found) *catchall route*
 app.use((req, res) => {
     res.status(404).end();
 });
