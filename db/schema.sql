60ad7a80461d08520e682cfe55182728343fceeb
TraBee <tra8brown@gmail.com>
2021-08-08 16:18:14 -0600

    updating things

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,8 @@
+DROP DATABASE IF EXISTS candidates_db;
+CREATE DATABASE candidates_db;
+
+USE candidates_db;
+
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
 
@@ -7,11 +12,6 @@ CREATE TABLE parties (
   description TEXT
 );
 
-INSERT INTO parties (name, description)
-VALUES
-  ('JS Juggernauts', 'The JS Juggernauts eat, breathe, and sleep JavaScript. They can build everything you could ever want in JS, including a new kitchen sink.'),
-  ('Heroes of HTML', 'Want to see a mock-up turn into an actual webpage in a matter of minutes? Well, the Heroes of HTML can get it done in a matter of seconds.'),
-  ('Git Gurus', 'Need to resolve a merge conflict? The Git Gurus have your back. Nobody knows Git like these folks do.');
 
 CREATE TABLE candidates (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
