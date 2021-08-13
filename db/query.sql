ce39b28f28b53239a9525a627c2dffafc211c141
TraBee <tra8brown@gmail.com>
2021-08-13 09:51:16 -0600

    test

---
+++ db/query.sql
@@ -0,0 +1,11 @@
+-- SELECT candidates.*, parties.* FROM candidates left join parties on candidates.party_id=parties.id;
+
+-- SELECT candidates.*, parties.* FROM candidates inner join parties on candidates.party_id=parties.id;
+
+
+-- SELECT candidates.*, parties.* FROM candidates right join parties on candidates.party_id=parties.id;
+
+
+SELECT * FROM candidates;
+
+SELECT * FROM parties;
