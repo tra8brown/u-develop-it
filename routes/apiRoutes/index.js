c00e502981a19b2721a68ba1be978131089a985e
TraBee <tra8brown@gmail.com>
2021-08-16 22:08:10 -0600

    added routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -9,6 +9,8 @@ router.use(require('./partyRoutes'));
 
 router.use(require('./voterRoutes'));
 
+router.use(require('./voteRoutes'));
+
 module.exports = router;
 
 //idex.jsfile will act as a central hub to pull them all together.
